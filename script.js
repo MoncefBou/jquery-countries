66a02989d269bc0450eb626e322a73804064c9c7
HD M2S <hdm2s@macbook-pro-de-hd.home>
2021-04-06 17:41:09 +0200

    ex en cours

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
 getCountries = () => {
-    
-    if ( )
-    $.ajax({
-        url: 'http://localhost:8000/countries',
-        success: (data, status, response) => {
 
-            for (i = 0; i < data.length; i++) {
-                $('ul').append(`<li>${data[i]}</li>`);
+        $.ajax({
+            url: 'http://localhost:8000/countries',
+            success: (data, status, response) => {
+               
+                $('ul').empty();
+
+                for (i = 0; i < data.length; i++) {
+                    $('ul').append(`<li>${data[i]}</li>`);
+                }
             }
-        }
-    })
+        })
 }
 
-$( () => {
+$(() => {
 
     $('#btnShowCountries').after('<ul></ul>');
 
